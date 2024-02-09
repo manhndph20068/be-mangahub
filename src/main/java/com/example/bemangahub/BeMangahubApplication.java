23524b4b7123b0c245115ca80bbbe311bd4ac9b0
manh <manhndph20068@fpt.edu.vn>
2024-02-09 15:43:42 +0700

    Initial commit

---
+++ src/main/java/com/example/bemangahub/BeMangahubApplication.java
@@ -0,0 +1,13 @@
+package com.example.bemangahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMangahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMangahubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bemangahub/BeMangahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bemangahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMangahubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
